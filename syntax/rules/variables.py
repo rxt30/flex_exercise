fb67ad681eacd2202eff8c074c096e3a9198585a
rxt30 <36088096+rxt30@users.noreply.github.com>
2021-05-03 15:00:31 +0200

    Added reassignment support

--- syntax/rules/variables.py
+++ syntax/rules/variables.py
@@ -4,7 +4,8 @@ def p_variable_int(p):
     'variable : INTEGER CHARS ASSIGNMENT expression'
     if type(p[4]) is not int:
         print("Wrong dataType")
-        return
+        print("Syntax error on line " + str(p.lineno(1)) + "\n")
+        raise SyntaxError
     savedVariables.update({p[2] : p[4]})
     print(savedVariables)
 
@@ -12,7 +13,8 @@ def p_variable_string(p):
     'variable : STRING CHARS ASSIGNMENT QUOTED_CHARS'
     if type(p[4]) is not str:
         print("Wrong dataType for Assignment")
-        return
+        print("Syntax error on line " + str(p.lineno(1)) + "\n")
+        raise SyntaxError
     savedVariables.update({p[2] : p[4].strip("'\"")})
     print(savedVariables)
 
@@ -20,4 +22,17 @@ def p_variable_bool(p):
     '''variable : BOOLEAN CHARS ASSIGNMENT TRUE
                 | BOOLEAN CHARS ASSIGNMENT FALSE'''
     savedVariables.update({p[2] : p[4] == "true"})
+    print(savedVariables)
+
+def p_variable_reassign(p):
+    '''variable : CHARS ASSIGNMENT expression
+                | CHARS ASSIGNMENT QUOTED_CHARS
+                | CHARS ASSIGNMENT TRUE
+                | CHARS ASSIGNMENT FALSE'''
+    if p[1] in savedVariables and type(p[3]) == type(savedVariables[p[1]]):
+        savedVariables.update({p[1] : p[3]})
+    else:
+        print("Wrong dataType for reassignment")
+        print("Syntax error on line " + str(p.lineno(1)) + "\n")
+        raise SyntaxError
     print(savedVariables)
