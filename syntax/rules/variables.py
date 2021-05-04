bd36f7a3097c32751623bbce00fc1345b4f560c8
rxt30 <36088096+rxt30@users.noreply.github.com>
2021-05-04 09:03:25 +0200

    Added reassignment support for bool

--- syntax/rules/variables.py
+++ syntax/rules/variables.py
@@ -26,9 +26,7 @@ def p_variable_bool(p):
 
 def p_variable_reassign(p):
     '''variable : CHARS ASSIGNMENT expression
-                | CHARS ASSIGNMENT QUOTED_CHARS
-                | CHARS ASSIGNMENT TRUE
-                | CHARS ASSIGNMENT FALSE'''
+                | CHARS ASSIGNMENT QUOTED_CHARS'''
     if not (p[1] in savedVariables and type(p[3]) == type(savedVariables[p[1]])):
         print("Wrong dataType for reassignment")
         print("Syntax error on line " + str(p.lineno(1)) + "\n")
@@ -37,9 +35,17 @@ def p_variable_reassign(p):
     savedVariableType = type(savedVariables[p[1]])
     if savedVariableType is str:
         savedVariables.update({p[1] : p[3].strip("'\"")})
-    elif savedVariableType is bool:
-        savedVariables.update({p[1] : p[3] == "true"})
     elif savedVariableType is int:
         savedVariables.update({p[1] : p[3]})
 
     print(savedVariables)
+
+def p_variable_reassign_bool(p):
+    '''variable : CHARS ASSIGNMENT TRUE
+                | CHARS ASSIGNMENT FALSE'''
+    if not (p[1] in savedVariables and type(savedVariables[p[1]]) is bool):
+        print("Wrong dataType for reassignment")
+        print("Syntax error on line " + str(p.lineno(1)) + "\n")
+        raise SyntaxError
+    else:
+        savedVariables.update({p[1] : p[3] == "true"})
