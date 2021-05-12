8cf3e09d52fcbbfdb98ef248d8708e8dcf3d4d55
rxt30 <36088096+rxt30@users.noreply.github.com>
2021-05-12 11:35:23 +0200

    Improved variable concat

--- syntax/rules/varConcat.py
+++ syntax/rules/varConcat.py
@@ -1,12 +1,16 @@
 from syntax.rules.savedVariables import *
 
 def p_varConcat_plus(p):
-    'varConcat : CHARS PLUS varConcat'
-    p[0] = getVariable(p[1], p.lineno(1)) + p[3]
+    'varConcat : varConcat PLUS varConcat'
+    p[0] = p[1] + p[3]
 
 def p_varConcat_chars(p):
     'varConcat : CHARS'
     p[0] = getVariable(p[1], p.lineno(1))
+
+def p_varConcat_expression(p):
+    'varConcat : expression'
+    p[0] = p[1]
 
 def p_varConcat_plus_expression_pre(p):
     'varConcat : varConcat PLUS expression'
