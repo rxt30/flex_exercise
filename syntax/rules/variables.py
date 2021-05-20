7ab98e1cf1d95f8d679b6378f1efa392ba354b6d
rxt30 <36088096+rxt30@users.noreply.github.com>
2021-05-20 15:56:24 +0200

    Added the declaration of floating point

--- syntax/rules/variables.py
+++ syntax/rules/variables.py
@@ -3,19 +3,22 @@ from syntax.rules.savedVariables import savedVariables
 
 def p_variable_int(p):
     '''variable : INTEGER CHARS ASSIGNMENT expression'''
-    if type(p[4]) is not int:
-        print("Wrong dataType")
-        print("Syntax error on line " + str(p.lineno(1)) + "\n")
-        raise SyntaxError
-    savedVariables.update({p[2] : p[4]})
+    if type(p[4]) is not float:
+        wrong_assignment_error(p)
+    savedVariables.update({p[2] : int(p[4])})
+    print(savedVariables)
+
+def p_variable_float(p):
+    'variable : FLOAT CHARS ASSIGNMENT expression'
+    if type(p[4]) is not float:
+        wrong_assignment_error(p)
+    savedVariables.update({p[2] : float(p[4])})
     print(savedVariables)
 
 def p_variable_string(p):
     'variable : STRING CHARS ASSIGNMENT expression'
     if type(p[4]) is not str:
-        print("Wrong dataType for Assignment")
-        print("Syntax error on line " + str(p.lineno(1)) + "\n")
-        raise SyntaxError
+        wrong_assignment_error(p)
     savedVariables.update({p[2] : p[4].strip("'\"")})
     print(savedVariables)
 
@@ -28,9 +31,7 @@ def p_variable_bool(p):
 def p_variable_varConcat(p):
     'variable : CHARS ASSIGNMENT expression'
     if not (p[1] in savedVariables and type(p[3]) == type(savedVariables[p[1]])):
-        print("Wrong dataType for reassignment")
-        print("Syntax error on line " + str(p.lineno(1)) + "\n")
-        raise SyntaxError
+        wrong_reassignment_error(p)
 
     savedVariableType = type(savedVariables[p[1]])
     if savedVariableType is str:
@@ -44,9 +45,17 @@ def p_variable_reassign_bool(p):
     '''variable : CHARS ASSIGNMENT TRUE
                 | CHARS ASSIGNMENT FALSE'''
     if not (p[1] in savedVariables and type(savedVariables[p[1]]) is bool):
-        print("Wrong dataType for reassignment")
-        print("Syntax error on line " + str(p.lineno(1)) + "\n")
-        raise SyntaxError
-    else:
-        savedVariables.update({p[1] : p[3] == "true"})
-        print(savedVariables)
+        wrong_reassignment_error(p)
+    savedVariables.update({p[1] : p[3] == "true"})
+    print(savedVariables)
+
+
+def wrong_assignment_error(p):
+    print("Wrong dataType for assignment")
+    print("Syntax error on line " + str(p.lineno(1)) + "\n")
+    raise SyntaxError
+
+def wrong_reassignment_error(p):
+    print("Wrong dataType for reassignment")
+    print("Syntax error on line " + str(p.lineno(1)) + "\n")
+    raise SyntaxError
